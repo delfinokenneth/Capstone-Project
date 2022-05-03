1d1374a55ede14ff2ee61d095c108cdd276baecb
Kenneth Delfino <kenneth.delfino11@gmail.com>
2022-05-03 17:22:16 +0800

    Fix same avg summary and report

--- Demo/Demo.py
+++ Demo/Demo.py
@@ -505,8 +505,8 @@ with app.app_context():
             ("negAve", negAve),
             ("neuAve", neuAve),
         ]
-        resp = requests.post('http://127.0.0.6:8000/reportGeneration', json = data, stream=True)
-        #resp = requests.post('https://csentiment-api.herokuapp.com/reportGeneration', json=data, stream=True)
+        #resp = requests.post('http://127.0.0.6:8000/reportGeneration', json = data, stream=True)
+        resp = requests.post('https://csentiment-api.herokuapp.com/reportGeneration', json=data, stream=True)
         return resp.raw.read(), resp.status_code, resp.headers.items()
 
 if __name__ == "__main__":
